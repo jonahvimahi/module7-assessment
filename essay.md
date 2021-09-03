9441a4861a02386ee7bf4c9a61eb3e6878edaff0
jonahvimahi <jonahvimahi@gmail.com>
2021-09-03 10:33:52 -0600

    first commit

---
+++ essay.md
@@ -0,0 +1,3 @@
+https://docs.google.com/document/d/1BoBabmrt-rI7RIvO9f6THXaAcIH6Mt3qnYylvemX3bQ/edit?usp=sharing
+
+link for the writing part of the assessment
---
+++ part3.js
@@ -0,0 +1,15 @@
+function pangram(s) {
+  let alphabet = "abcdefghijklmnopqrstuvwxyz";
+  let regex = /\s/g;
+  let lowercase = s.toLowerCase().replace(regex, "");
+
+  for (let i = 0; i < alphabet.length; i++) {
+    if (lowercase.indexOf(alphabet[i]) === -1) {
+      console.log('not a pangram');
+    }
+  }
+  console.log("pangram");
+}
+// pangram("The quick brown fox jumps over the lazy dog")
+
+pangram("I like cats but not mice")
